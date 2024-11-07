89963d48482d898268e665dee7ecc8c8b184b307
DavidClase <dperezfernandez@safareyes.es>
2024-11-07 16:42:04 +0100

    🎉 Initialize project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Button, ButtonDirective, ButtonModule} from 'primeng/button';
+import {Ripple} from 'primeng/ripple';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Button, ButtonDirective, Ripple],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'GoodGather-Frontend';
