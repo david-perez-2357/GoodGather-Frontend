8e4d560469f50433f7488d3027026f5b8c74a197
DavidClase <dperezfernandez@safareyes.es>
2024-11-08 17:08:05 +0100

    💄 Add header to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Button, ButtonDirective, ButtonModule} from 'primeng/button';
 import {Ripple} from 'primeng/ripple';
+import {HeaderComponent} from '../component/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Button, ButtonDirective, Ripple],
+  imports: [RouterOutlet, Button, ButtonDirective, Ripple, HeaderComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
