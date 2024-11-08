bafe55a0cd8b6bc6188385d2b274bc770f4def4c
DavidClase <dperezfernandez@safareyes.es>
2024-11-08 17:07:45 +0100

    💄 Created header component

---
+++ src/component/header/header.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import {MenubarModule} from 'primeng/menubar';
+import {MenuItem} from 'primeng/api';
+import {ChipsModule} from 'primeng/chips';
+import {NgClass, NgIf} from '@angular/common';
+import {AvatarModule} from 'primeng/avatar';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    MenubarModule,
+    ChipsModule,
+    NgClass,
+    NgIf,
+    AvatarModule,
+  ],
+  templateUrl: './header.component.html',
+  styles: ``
+})
+export class HeaderComponent {
+  items: MenuItem[] = [
+    {
+      label: 'Inicio',
+      icon: 'pi pi-home',
+      routerLink: ['/']
+    },
+    {
+      label: 'Organizar un evento',
+      icon: 'pi pi-calendar-plus',
+      routerLink: ['/organize-event']
+    },
+  ];
+
+}
