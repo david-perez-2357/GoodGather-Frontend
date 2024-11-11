1040501ddde56b0a6ab5c9a0163aef051e461ee0
DavidClase <dperezfernandez@safareyes.es>
2024-11-11 20:49:41 +0100

    🚚 Upload form bg to assets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import {PageNotFoundComponent} from '../page/page-not-found/page-not-found.component';
+import {RegisterAndLoginComponent} from '../page/register-and-login/register-and-login.component';
 
 export const routes: Routes = [
+  {'path': 'register', 'component': RegisterAndLoginComponent},
+  {'path': 'login', 'component': RegisterAndLoginComponent},
   {'path': '**', 'component': PageNotFoundComponent}
 ];
