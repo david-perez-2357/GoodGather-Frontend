584298b86866c0309278b0fa1694e3dd062ac763
Jorge Arias <jariasmartin@safareyes.es>
2024-11-12 00:01:13 +0100

    ✨ Add form background to be global

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {PageNotFoundComponent} from '../page/page-not-found/page-not-found.component';
+import {CreateEventComponent} from '../page/create-event/create-event.component';
 
 export const routes: Routes = [
+  {'path': 'organize-event', 'component': CreateEventComponent },
   {'path': '**', 'component': PageNotFoundComponent}
 ];
