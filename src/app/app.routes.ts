04140618d2c8972d50681c447ed6347d916d85b5
Jorge Arias <jariasmartin@safareyes.es>
2024-11-12 00:03:27 +0100

    🔥 Remove unresolved path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,7 @@
 import { Routes } from '@angular/router';
 import {PageNotFoundComponent} from '../page/page-not-found/page-not-found.component';
-import {CreateEventComponent} from '../page/create-event/create-event.component';
+
 
 export const routes: Routes = [
-  {'path': 'organize-event', 'component': CreateEventComponent },
   {'path': '**', 'component': PageNotFoundComponent}
 ];
