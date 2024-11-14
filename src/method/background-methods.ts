609528ce2a2795931239c3f28edcb62c1103a4b1
david <dperezfernandez@safareyes.es>
2024-11-14 13:19:37 +0100

    ✨ Add remove background methods and create an interface for backgrounds

--- src/method/background-methods.ts
+++ src/method/background-methods.ts
@@ -1,22 +1,47 @@
-import {ElementRef, Renderer2} from '@angular/core';
+import {Renderer2} from '@angular/core';
+import Background from '../interface/Background';
 
-function putBackground(renderer: Renderer2, imgSrc: string) {
-  renderer.addClass(document.body, 'default-bg');
+
+const defaultBackground: Background = {
+  image: 'gg-default-background.png',
+  className: 'default-bg'
+}
+
+const formBackground: Background = {
+  image: 'gg-form-background.png',
+  className: 'form-bg'
+}
+
+function putBackground(renderer: Renderer2,Background: Background) {
+  renderer.addClass(document.body, Background.className);
 
   const img = new Image();
-  img.src = imgSrc;
+  img.src = Background.image;
 
   img.onload = () => {
     renderer.setStyle(document.body, 'backgroundImage', `url(${img.src})`);
   };
 }
 
+function removeBackgroundAndClass(renderer: Renderer2, Background: Background) {
+  renderer.removeStyle(document.body, 'backgroundImage');
+  renderer.removeClass(document.body, Background.className);
+}
+
 function putDefaultBackground(renderer: Renderer2) {
-  putBackground(renderer, 'gg-default-background.png');
+  putBackground(renderer, defaultBackground);
 }
 
 function putFormBackground(renderer: Renderer2) {
-  putBackground(renderer, 'gg-form-background.png');
+  putBackground(renderer, formBackground);
 }
 
-export {putDefaultBackground, putFormBackground};
+function removeDefaultBackground(renderer: Renderer2) {
+  removeBackgroundAndClass(renderer, defaultBackground);
+}
+
+function removeFormBackground(renderer: Renderer2) {
+  removeBackgroundAndClass(renderer, formBackground);
+}
+
+export {putDefaultBackground, putFormBackground, removeDefaultBackground, removeFormBackground};
