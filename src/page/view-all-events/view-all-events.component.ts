ce46a5c513ff24a43b8e7bbe0cf6665f656524c6
Jorge Arias <jariasmartin@safareyes.es>
2024-11-15 17:35:55 +0100

    ✨ Create frontend for view-all-events page

--- src/page/view-all-events/view-all-events.component.ts
+++ src/page/view-all-events/view-all-events.component.ts
@@ -1,10 +1,18 @@
-import {Component, Renderer2} from '@angular/core';
-import {FloatLabelModule} from 'primeng/floatlabel';
-import {FormsModule} from '@angular/forms';
-import {ChipsModule} from 'primeng/chips';
-import {Button} from 'primeng/button';
-import {IconFieldModule} from 'primeng/iconfield';
-import {InputIconModule} from 'primeng/inputicon';
+import { Component, Renderer2, ViewChild } from '@angular/core';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { FormsModule } from '@angular/forms';
+import { ChipsModule } from 'primeng/chips';
+import { Button, ButtonDirective } from 'primeng/button';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { CheckboxModule } from 'primeng/checkbox';
+import { NgForOf } from '@angular/common';
+import { OverlayPanel } from 'primeng/overlaypanel';
+import {SelectButtonModule} from 'primeng/selectbutton';
+import {SliderModule} from 'primeng/slider';
+import {PaginatorModule} from 'primeng/paginator';
+import {Ripple} from 'primeng/ripple';
 
 @Component({
   selector: 'app-page-index',
@@ -15,14 +23,29 @@ import {InputIconModule} from 'primeng/inputicon';
     ChipsModule,
     Button,
     IconFieldModule,
-    InputIconModule
+    InputIconModule,
+    OverlayPanelModule,
+    CheckboxModule,
+    NgForOf,
+    ButtonDirective,
+    SelectButtonModule,
+    SliderModule,
+    PaginatorModule,
+    Ripple
   ],
   templateUrl: './view-all-events.component.html',
-  styles: ``
+  styleUrls: ['./view-all-events.component.css']
 })
 export class ViewAllEventsComponent {
-  value: any;
-  loading: unknown;
+  @ViewChild('overlay') overlay!: OverlayPanel;
+
+  loading = false;
+  stateOptions: any[] = [{ label: 'En tu país', value: 'country' }, { label: 'En tu provincia', value: 'province' }];
+
+  value: string = 'province';
+
+  valueslider: number = 50;
+
   constructor(private renderer: Renderer2) {}
 
   ngOnInit(): void {
@@ -34,6 +57,14 @@ export class ViewAllEventsComponent {
   }
 
   load() {
-
+    this.loading = true;
+    setTimeout(() => {
+      this.loading = false;
+    }, 1000);
   }
+
+  toggleOverlay(event: Event) {
+    this.overlay.toggle(event);
+  }
+
 }
