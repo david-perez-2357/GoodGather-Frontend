34e775cbab91cd77e4cab9a47a7d108f410c27e8
Mario Castro <mcrcdr@gmail.com>
2024-11-15 16:06:25 +0100

    ✨ Routed organize event page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import {PageNotFoundComponent} from '../page/page-not-found/page-not-found.component';
+import {OrganizeEventComponent} from '../page/organize-event/organize-event.component';
 import {EventDetailsComponent} from '../page/event-details/event-details.component';
 
 
 export const routes: Routes = [
   {'path': 'event/:id', component: EventDetailsComponent},
+  {'path': 'organize-event', 'component': OrganizeEventComponent},
   {'path': '**', 'component': PageNotFoundComponent}
 ];
