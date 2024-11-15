efd7b34ddf74ab80c82a6297b7385e8b7feb7756
david <dperezfernandez@safareyes.es>
2024-11-15 12:30:58 +0100

    ✨ Implement get event from backend

--- src/page/event-details/event-details.component.ts
+++ src/page/event-details/event-details.component.ts
@@ -4,7 +4,14 @@ import {BuyTicketButtonComponent} from '../../component/buy-ticket-button/buy-ti
 import {ProgressBarModule} from 'primeng/progressbar';
 import {putDefaultBackground, removeDefaultBackground} from '../../method/background-methods';
 import Event from '../../interface/Event';
-import {convertSecondsToString} from '../../method/date-methods';
+import {EventService} from '../../service/EventService';
+import { HttpClientModule } from '@angular/common/http';
+import moment from 'moment';
+import {SkeletonModule} from 'primeng/skeleton';
+import {NgIf} from '@angular/common';
+import {handleResponse} from '../../method/response-mehods';
+import {MessageService} from 'primeng/api';
+import {ToastModule} from 'primeng/toast';
 
 @Component({
   selector: 'app-event-details',
@@ -12,67 +19,93 @@ import {convertSecondsToString} from '../../method/date-methods';
   imports: [
     CardModule,
     BuyTicketButtonComponent,
-    ProgressBarModule
+    ProgressBarModule,
+    HttpClientModule,
+    SkeletonModule,
+    NgIf,
+    ToastModule,
   ],
   templateUrl: './event-details.component.html',
-  styles: ``
+  styles: ``,
 })
 
 export class EventDetailsComponent implements OnInit, OnDestroy {
+  eventLoaded: boolean = false;
   event: Event = {
     id: 0,
     name: 'Event Name',
     description: 'Event Description',
     image: 'https://hips.hearstapps.com/hmg-prod/images/calendario-carreras-populares-running-sevilla-2023-1669049504.jpg',
-    start_date: '2023-01-23 00:00:00',
-    end_date: '2024-01-30 13:00:00',
+    startDate: '2023-01-23 00:00:00',
+    endDate: '2024-01-30 13:00:00',
     capacity: 100,
-    bought_tickets: 25,
+    boughtTickets: 25,
     address: 'The Address',
     province: 'The Province',
     country: 'The Country',
-    ticket_price: 0,
+    ticketPrice: 0,
     deleted: 0,
-    id_owner: 0,
-    id_cause: 0
+    idOwner: 0,
+    idCause: 0
   }
 
-  constructor(private renderer: Renderer2) {}
+  constructor(private renderer: Renderer2, private eventService: EventService, private messageService: MessageService) {}
 
   calculateTicketPercentage() {
-    return (this.event.bought_tickets / this.event.capacity) * 100;
+    return (this.event.boughtTickets / this.event.capacity) * 100;
   }
 
-  getDateRange() {
-    const startDate = new Date(this.event.start_date);
-    const endDate = new Date(this.event.end_date);
+  getDateRange(): string {
+    // Convierte las fechas usando Moment.js, especificando el formato de entrada
+    const startDate = moment(this.event.startDate, "YYYY-MM-DD HH:mm");
+    const endDate = moment(this.event.endDate, "YYYY-MM-DD HH:mm");
 
-    const formattedStartDate = `${startDate.getDate()}/${startDate.getMonth() + 1}/${startDate.getFullYear()}`;
-    const formattedEndDate = `${endDate.getDate()}/${endDate.getMonth() + 1}/${endDate.getFullYear()}`;
+    console.log('startDate:', this.event.startDate);
 
-    const startHour = startDate.getHours() < 10 ? `0${startDate.getHours()}` : startDate.getHours();
-    const startMinute = startDate.getMinutes() < 10 ? `0${startDate.getMinutes()}` : startDate.getMinutes();
+    // Usa Moment.js para el formateo de fecha y hora
+    const formattedStartDate = startDate.format("DD/MM/YYYY");
+    const formattedEndDate = endDate.format("DD/MM/YYYY");
 
-    const endHour = endDate.getHours() < 10 ? `0${endDate.getHours()}` : endDate.getHours();
-    const endMinute = endDate.getMinutes() < 10 ? `0${endDate.getMinutes()}` : endDate.getMinutes();
+    const startHour = startDate.format("HH:mm");
+    const endHour = endDate.format("HH:mm");
 
-    if (startDate.getDate() === endDate.getDate()) {
-      // Si estan en el mismo dia poner como 12/12/2023 00:00 - 01:00
-      return `${formattedStartDate} ${startHour}:${startMinute} - ${endHour}:${endMinute}`;
-    } else if (startDate.getMonth() === endDate.getMonth() && startDate.getFullYear() === endDate.getFullYear() && startDate.getFullYear() === new Date().getFullYear()) {
-      // Si estan en el mismo mes y año, poner Del 12 de diciembre, 00:00, al 13 de diciembre, 01:00
-      return `Del ${startDate.getDate()} de ${startDate.toLocaleString('es-ES', { month: 'long' })}, a las ${startHour}:${startMinute}, al ${endDate.getDate()} de ${endDate.toLocaleString('es-ES', { month: 'long' })}, a las ${endHour}:${endMinute}`;
+    if (startDate.isSame(endDate, 'day')) {
+      // Si están en el mismo día, muestra como: 12/12/2023 00:00 - 01:00
+      return `${formattedStartDate} ${startHour} - ${endHour}`;
+    } else if (startDate.isSame(endDate, 'month') && startDate.isSame(endDate, 'year') && startDate.isSame(moment(), 'year')) {
+      // Si están en el mismo mes y año, muestra como: Del 12 de diciembre, 00:00, al 13 de diciembre, 01:00
+      return `Del ${startDate.date()} de ${startDate.format("MMMM")}, a las ${startHour}, al ${endDate.date()} de ${endDate.format("MMMM")}, a las ${endHour}`;
     } else {
-      // Si no poner Desde el 12/12/2023 a las 00:00 hasta el 13/12/2023 a las 01:00
-      return `Desde el ${formattedStartDate} a las ${startHour}:${startMinute} hasta el ${formattedEndDate} a las ${endHour}:${endMinute}`;
+      // En otros casos, muestra como: Desde el 12/12/2023 a las 00:00 hasta el 13/12/2023 a las 01:00
+      return `Desde el ${formattedStartDate} a las ${startHour} hasta el ${formattedEndDate} a las ${endHour}`;
     }
+  }
+
+
+
+  getEventFromAPI() {
+    this.eventService.getEvent(1).subscribe({
+      next: (response: Event) => {
+        this.event = response;
+        this.eventLoaded = true;
+      },
+      error: (error: any) => {
+        this.messageService.add({ severity: 'error', summary: 'Error al obtener datos', detail: error.message });
+      }
+    });
   }
 
   ngOnInit() {
     putDefaultBackground(this.renderer);
+    this.getEventFromAPI();
   }
 
   ngOnDestroy() {
     removeDefaultBackground(this.renderer);
+  }
+
+  onImageError($event: ErrorEvent) {
+    const target = $event.target as HTMLImageElement;
+    target.src = 'gg-placeholder-image.png';
   }
 }
