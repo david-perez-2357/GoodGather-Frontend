fbd9d90de229acf38d155be63a9cec2be8cba195
DavidClase <dperezfernandez@safareyes.es>
2024-11-15 15:52:00 +0100

    ✨ Created method to call API

--- src/method/response-mehods.ts
+++ src/method/response-mehods.ts
@@ -1,8 +1,35 @@
+import {Message} from 'primeng/api';
+import ApiResponse from '../interface/ApiResponse';
+import {firstValueFrom, Observable} from 'rxjs';
 
-function handleResponse(response: any) {
-  if (response.status !== 200) {
-    console.error('Error:', response);
+function callAPI(serviceCall: any): Promise<ApiResponse> {
+  return firstValueFrom(serviceCall).then(
+    (response: any) => ({
+      status: response.status,
+      message: response.message,
+      data: response.data,
+    }),
+    (error: any) => ({
+      status: error.status,
+      message: error.message,
+      data: error.data,
+      toastMessage: returnErrorMessage(error),
+    })
+  );
+}
+
+function returnErrorMessage(error: any): Message {
+  if (error.status === 404) {
+    return {severity: 'error', summary: 'No se han encontrado datos', detail: 'Por favor, inténtelo de nuevo más tarde.'};
+  }else if (error.status === 500) {
+    return {severity: 'error', summary: 'Error en el servidor', detail: 'Por favor, inténtelo de nuevo más tarde.'};
+  }else if (error.status === 400) {
+    return {severity: 'error', summary: 'Error en la petición', detail: 'Por favor, inténtelo de nuevo más tarde.'};
+  }else if (error.status === 403) {
+    return {severity: 'error', summary: 'Acceso denegado', detail: 'Por favor, inicie sesión e inténtelo de nuevo.'};
+  }else {
+    return {severity: 'error', summary: 'Error', detail: 'Por favor, inténtelo de nuevo más tarde.'};
   }
 }
 
-export { handleResponse };
+export {callAPI, returnErrorMessage};
