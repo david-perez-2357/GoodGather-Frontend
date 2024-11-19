192b9509b0242fa588195ef56a4490cecf911754
Mario Castro <mcrcdr@gmail.com>
2024-11-19 16:34:17 +0100

    🚧 organize event page stepper validating

--- src/page/organize-event/organize-event.component.ts
+++ src/page/organize-event/organize-event.component.ts
@@ -6,6 +6,11 @@ import {InputTextareaModule} from 'primeng/inputtextarea';
 import {DropdownModule} from 'primeng/dropdown';
 import {FormsModule} from '@angular/forms';
 import {TooltipModule} from 'primeng/tooltip';
+import moment from 'moment';
+import Event from '../../interface/Event';
+import {putFormBackground, removeFormBackground} from '../../method/background-methods';
+import {NgIf} from '@angular/common';
+import {InputNumberModule} from 'primeng/inputnumber';
 
 @Component({
   selector: 'app-organize-event',
@@ -17,25 +22,56 @@ import {TooltipModule} from 'primeng/tooltip';
     InputTextModule,
     DropdownModule,
     FormsModule,
-    TooltipModule
+    TooltipModule,
+    NgIf,
+    InputNumberModule
   ],
   templateUrl: './organize-event.component.html',
   styles: ``
 })
 export class OrganizeEventComponent implements OnInit, OnDestroy {
   causes: any[] | undefined;
-  selectedCause: any;
   countries: any[] | undefined;
-  selectedCountry: any;
   province: any[] | undefined;
-  selectedProvince: any;
+  startDateDate: string = '';
+  endDateDate: string = '';
+  startDateHour: string = '';
+  endDateHour: string = '';
+
+  event: Event = {
+    id: 0,
+    name: '',
+    description: '',
+    image: '',
+    startDate: '',
+    endDate: '',
+    capacity: 0,
+    boughtTickets: 0,
+    address: '',
+    province: '',
+    country: '',
+    ticketPrice: 0,
+    deleted: 0,
+    idOwner: 0,
+    idCause: 0
+  }
   constructor(private renderer: Renderer2) {}
 
   ngOnInit(): void {
-    this.renderer.addClass(document.body, 'form-bg');
+    putFormBackground(this.renderer);
+    this.startDateDate = moment().format('YYYY-MM-DD');
+    this.endDateDate = this.startDateDate;
+    if (this.startDateDate == this.endDateDate && this.endDateHour < this.startDateHour) {
+      this.endDateHour = this.startDateHour;
+    }
   }
 
   ngOnDestroy(): void {
-    this.renderer.removeClass(document.body, 'form-bg');
+    removeFormBackground(this.renderer);
   }
+
+  startDateChanged() {
+    this.endDateDate = this.event.startDate;
+  }
+
 }
