7357948adcafab85466a47794583f2fc4bba9b56
Jorge Arias <jariasmartin@safareyes.es>
2024-11-19 20:30:55 +0100

    ✨ Create getAllCauses function

---
+++ src/service/CauseService.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import Cause from '../interface/Cause';
+
+@Injectable({providedIn: 'root'})
+export class CauseService {
+  constructor(private http: HttpClient) {
+  }
+
+  getCause(id: number): Observable<Cause> {
+    return this.http.get<Cause>(`/api/cause/${id}`, { withCredentials: true });
+  }
+
+  getAllCauses(): Observable<Cause[]> {
+    return this.http.get<Cause[]>('/api/cause', { withCredentials: true });
+  }
+}
