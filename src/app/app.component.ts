d5474bab8864622f9da7ef5fa0a8bcf0a3f3b8aa
DavidClase <dperezfernandez@safareyes.es>
2024-11-19 20:16:47 +0100

    ✨ Add a function to toggle error component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {Button, ButtonDirective, ButtonModule} from 'primeng/button';
-import {Ripple} from 'primeng/ripple';
 import {HeaderComponent} from '../component/header/header.component';
+import {ServerErrorComponent} from '../component/server-error/server-error.component';
+import ApiResponse from '../interface/ApiResponse';
+import {NgIf} from '@angular/common';
+import {AppService} from '../service/AppService';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Button, ButtonDirective, Ripple, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ServerErrorComponent, NgIf],
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  title = 'GoodGather-Frontend';
+export class AppComponent implements OnInit {
+  title = 'GoodGather';
+  error = false;
+  errorText: string = 'Ha ocurrido un error en el servidor';
+
+  constructor(private appService: AppService) {}
+
+  showError(apiResponse: ApiResponse): void {
+    this.error = true;
+
+    if (apiResponse?.toastMessage?.summary) {
+      this.errorText = apiResponse.toastMessage.summary;
+    }
+
+    if (apiResponse.status == 403) {
+      window.location.href = '/login';
+    }
+  }
+
+  ngOnInit() {
+    this.appService.triggerFunction$.subscribe((apiResponse) => {
+      this.showError(apiResponse);
+    });
+  }
 }
