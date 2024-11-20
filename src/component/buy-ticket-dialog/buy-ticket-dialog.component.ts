decc2c49fd51b748125cd786a595f4fffdecbaae
DavidClase <dperezfernandez@safareyes.es>
2024-11-20 21:07:33 +0100

    🚧 Create buy ticket dialog component

---
+++ src/component/buy-ticket-dialog/buy-ticket-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buy-ticket-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './buy-ticket-dialog.component.html',
+  styles: ``
+})
+export class BuyTicketDialogComponent {
+
+}
