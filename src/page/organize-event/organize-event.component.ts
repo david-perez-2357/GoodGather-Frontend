03eacbf6f8e0ac468a54c424a411a4b05f4a3eb6
Mario Castro <mcrcdr@gmail.com>
2024-11-21 20:50:57 +0100

    ✏️ Changed type deleted

--- src/page/organize-event/organize-event.component.ts
+++ src/page/organize-event/organize-event.component.ts
@@ -60,7 +60,7 @@ export class OrganizeEventComponent implements OnInit, OnDestroy {
     province: '',
     country: '',
     ticketPrice: 0,
-    deleted: 0,
+    deleted: false,
     idOwner: 1,
     idCause: 1
   }
