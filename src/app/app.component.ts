3c368b2f72cdf0332a9d863c1db75e1ea228d4c3
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2024-11-22 17:04:46 +0100

    🔀 Merged app.component with development

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Button, ButtonDirective, ButtonModule} from 'primeng/button';
 import {Ripple} from 'primeng/ripple';
