3dfd7556659d9d976e5bed485f03e047bb215f9b
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2024-11-22 17:01:00 +0100

    ⚡️ Create new routes to login and register form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import {PageNotFoundComponent} from '../page/page-not-found/page-not-found.component';
 import {RegisterAndLoginComponent} from '../page/register-and-login/register-and-login.component';
 import {EventDetailsComponent} from '../page/event-details/event-details.component';
+import {ToggleComponent} from '../toggle/toggle.component';
+
 
 
 export const routes: Routes = [
   {'path': 'login', component: RegisterAndLoginComponent},
+  {'path': 'register', component: RegisterAndLoginComponent},
   {'path': 'event/:id', component: EventDetailsComponent},
-  {'path': '**', 'component': PageNotFoundComponent}
+  {'path': 'toggle', component: ToggleComponent},
+  {'path': '**', 'component': PageNotFoundComponent},
 ];
