5439f0af892368a932f4b1cc01c12dba39c7f0f9
DavidClase <dperezfernandez@safareyes.es>
2024-11-22 20:40:23 +0100

    ✨ Add new fields for paying

--- src/component/buy-ticket-dialog/buy-ticket-dialog.component.ts
+++ src/component/buy-ticket-dialog/buy-ticket-dialog.component.ts
@@ -4,7 +4,7 @@ import {Button} from 'primeng/button';
 import {InputTextModule} from 'primeng/inputtext';
 import {AvatarModule} from 'primeng/avatar';
 import {StepperModule} from 'primeng/stepper';
-import {NgClass, NgIf} from '@angular/common';
+import {NgClass, NgForOf, NgIf} from '@angular/common';
 import {IconFieldModule} from 'primeng/iconfield';
 import {InputIconModule} from 'primeng/inputicon';
 import {PasswordModule} from 'primeng/password';
@@ -14,6 +14,13 @@ import {BadgeModule} from 'primeng/badge';
 import {MessagesModule} from 'primeng/messages';
 import {InputNumberModule} from 'primeng/inputnumber';
 import {FormsModule} from '@angular/forms';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import {DropdownModule} from 'primeng/dropdown';
+import {InputMaskModule} from 'primeng/inputmask';
+import {PanelModule} from 'primeng/panel';
+import {BlockUIModule} from 'primeng/blockui';
+import {StyleClassModule} from 'primeng/styleclass';
+import moment from 'moment';
 
 @Component({
   selector: 'app-buy-ticket-dialog',
@@ -24,7 +31,6 @@ import {FormsModule} from '@angular/forms';
     InputTextModule,
     AvatarModule,
     StepperModule,
-    NgClass,
     IconFieldModule,
     InputIconModule,
     PasswordModule,
@@ -34,7 +40,14 @@ import {FormsModule} from '@angular/forms';
     MessagesModule,
     InputNumberModule,
     FormsModule,
-    NgIf
+    NgIf,
+    RadioButtonModule,
+    NgForOf,
+    DropdownModule,
+    InputMaskModule,
+    PanelModule,
+    BlockUIModule,
+    StyleClassModule
   ],
   templateUrl: './buy-ticket-dialog.component.html',
   styles: ``
@@ -42,6 +55,7 @@ import {FormsModule} from '@angular/forms';
 export class BuyTicketDialogComponent implements OnInit {
   @Input() visible: boolean = false;
   @Input() eventId: number = 0;
+  @Input() eventName: string = '';
   @Input() ticketPrice: number = 0;
   @Input() ticketsLeft: number = 10;
   @Output() closeTicketDialog = new EventEmitter<void>();
@@ -50,6 +64,30 @@ export class BuyTicketDialogComponent implements OnInit {
   maxQuantity: number = 10;
   ticketsBought: number = 4;
   currentMaxQuantity: number = 10;
+  paymentMethods = [
+    {label: 'Tarjeta de crédito', value: 'creditCard', icon: 'pi pi-credit-card'},
+    {label: 'Paypal', value: 'paypal', icon: 'pi pi-paypal'},
+  ];
+  selectedPaymentMethod: string = 'creditCard';
+
+  cardTypes = [
+    { value: "visa", label: "Visa", mask: "4 999 9999 9999 9999" },
+    { value: "mastercard", label: "Mastercard", mask: "5 999 9999 9999 9999" },
+    { value: "amex", label: "American Express", mask: "3 999 999999 99999" },
+    { value: "dinersclub", label: "Diners Club", mask: "3 999 999999 9999" },
+    { value: "discover", label: "Discover", mask: "6 999 9999 9999 9999" },
+    { value: "jcb", label: "JCB", mask: "3 999 9999 9999 9999" },
+    { value: "maestro", label: "Maestro", mask: "5 999 9999 9999 9999" }
+  ];
+
+  selectedCardType: { value: string, label: string, mask: string } = this.cardTypes[0];
+  accountNumber: string = '';
+  expirationDate: string = '';
+  cvv: number = 0;
+  paypalEmail: string = '';
+  paypalPassword: string = '';
+  currentDate: string = moment().format('DD/MM/YYYY');
+
 
   ngOnInit() {
     this.currentMaxQuantity = this.maxQuantity - this.ticketsBought;
@@ -59,5 +97,4 @@ export class BuyTicketDialogComponent implements OnInit {
     this.visible = false;
     this.closeTicketDialog.emit();
   }
-
 }
