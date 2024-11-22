b29807b947d72b22d6feb8d7e3d180198413ea41
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2024-11-22 17:02:01 +0100

    ⚡️ Create user creation interface (User.ts)

---
+++ src/interface/User.ts
@@ -0,0 +1,11 @@
+    interface User{
+      id:number;
+      username:string;
+      password:string;
+      email:string;
+      birthdate:string;
+      country:string;
+      province:string;
+    }
+
+    export default User
