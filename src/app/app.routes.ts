db28d4a2c2839c93f13ba6f517a86b9a46873089
Mario Castro <mcrcdr@gmail.com>
2024-11-26 16:35:03 +0100

    ♻️ Change relative routes to absolute

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,7 @@
 import { Routes } from '@angular/router';
 import {PageNotFoundComponent} from '@/page/page-not-found/page-not-found.component';
 import {EventDetailsComponent} from '@/page/event-details/event-details.component';
-import {OrganizeEventComponent} from '../page/organize-event/organize-event.component';
-
+import {OrganizeEventComponent} from '@/page/organize-event/organize-event.component';
 
 export const routes: Routes = [
   {'path': 'event/:id', component: EventDetailsComponent},
