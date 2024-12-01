1ff2ae1284d16f60a77cb994eef5dc314fd996e5
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2024-12-01 13:22:49 +0100

    ♻️ Change relative paths to absolute

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,15 +4,15 @@ import {ViewAllEventsComponent} from '@/page/view-all-events/view-all-events.com
 import {EventDetailsComponent} from '@/page/event-details/event-details.component';
 import {OrganizeEventComponent} from '@/page/organize-event/organize-event.component';
 import {RegisterAndLoginComponent} from '@/page/register-and-login/register-and-login.component';
-import {ToggleComponent} from '@/toggle/toggle.component';
+
+
 
 export const routes: Routes = [
+  {'path': '', component: ViewAllEventsComponent},
   {'path': 'login', component: RegisterAndLoginComponent},
   {'path': 'register', component: RegisterAndLoginComponent},
-  {'path': '', component: ViewAllEventsComponent},
   {'path': 'event/:id', component: EventDetailsComponent},
-  {'path': 'toggle', component: ToggleComponent},
-    {'path': '**', 'component': PageNotFoundComponent},
-  {'path': 'organize-event', 'component': OrganizeEventComponent},
+  {'path': 'organize-event', component: OrganizeEventComponent},
+  {'path': '**', component: PageNotFoundComponent},
 
 ];
