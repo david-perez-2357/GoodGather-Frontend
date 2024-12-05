d1b89c4b555a4e4cbcc220433b9bb200596ced47
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2024-12-05 19:40:08 +0100

    ✨ Add methods checking if username and email exist in backend

--- src/service/AuthService.ts
+++ src/service/AuthService.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import UserClient from '@/interface/UserClient';
+import User from '@/interface/User';
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
@@ -8,7 +10,34 @@ export class AuthService {
   }
 
   getCurrentUserFromServer(): Observable<any> {
-    this.http.post('/api/api/v1/auth/logout', {}, { withCredentials: true }).subscribe();
     return this.http.get('/api/api/v1/auth/user', { withCredentials: true });
   }
+
+  createUser(user:UserClient):Observable<any>{
+    return this.http.post('/api/api/v1/auth/register', user, { withCredentials: true });
+  }
+
+  doLogin(user:User):Observable<User> {
+    return this.http.post<User>('/api/api/v1/auth/authenticate', user,
+      { withCredentials: true});
+  }
+
+  doLogOut():Observable<any>{
+    return this.http.post('/api/api/v1/auth/logout', {},
+      { withCredentials: true, responseType: 'text' });
+
+  }
+
+  isUserExist(username:string):Observable<boolean>{
+    const params = new HttpParams().set('username', username);
+    return this.http.get<boolean>('api/api/v1/auth/ckeck-username-exists', {withCredentials: true, params: { username } });
+
+  }
+
+  isEmailExist(email:string):Observable<boolean>{
+    const params = new HttpParams().set('username', email);
+    return this.http.get<boolean>('api/api/v1/auth/ckeck-email-exists', {withCredentials: true, params: { email } });
+
+  }
+
 }
